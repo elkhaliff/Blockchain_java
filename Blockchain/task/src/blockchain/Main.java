2484ad1fa3c11aa121d9fd90408358f3a232dc66
master <elkhaliff@gmail.com>
2022-05-15 13:25:34 +0500

    3/6 добавил классы, в том числе серилизацию, пока ничего не работает.

---
+++ Blockchain/task/src/blockchain/Main.java
@@ -0,0 +1,18 @@
+package blockchain;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String prevHash = "0";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter how many zeros the hash must start with: ");
+        int zeroCnt = scanner.nextInt();
+        System.out.println();
+        for (int i = 1; i < 6; i++) {
+            Block block = new Block(i, zeroCnt, prevHash);
+            prevHash = block.getNewHash();
+            System.out.println(block);
+        }
+    }
+}
