badd31920dfe0626c3dbdcf1e6eb4cb60361f767
master <elkhaliff@gmail.com>
2022-05-15 13:23:47 +0500

    рефактор git

--- Blockchain/Topics/Inner classes/Your vehicle/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-class EnjoyVehicle {
-
-    public static void startVehicle() {
-        Vehicle vehicle = new Vehicle();
-        Vehicle.Engine engine = vehicle.new Engine();
-        engine.start();
-    }
-}
